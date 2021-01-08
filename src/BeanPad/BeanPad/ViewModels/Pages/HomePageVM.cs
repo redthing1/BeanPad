9e14a47876ee49dbd6d8ec476a5483bdfac3a80f
xdrie <xdrie@alt.icu>
2021-01-07 22:13:23 -0800

    add exit command

--- src/BeanPad/BeanPad/ViewModels/Pages/HomePageVM.cs
+++ src/BeanPad/BeanPad/ViewModels/Pages/HomePageVM.cs
@@ -1,5 +1,19 @@
+using System;
+using System.Windows.Input;
+using Avalonia.Controls.ApplicationLifetimes;
+using ReactiveUI;
+
 namespace BeanPad.ViewModels.Pages {
     public class HomePageVM : ViewModelBase {
         public string Greeting => "Hello, welcome to the world!";
+
+        public ICommand MenuExit { get; } = ReactiveCommand.Create(() => {
+            var lifetime = App.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime;
+            if (lifetime == null) {
+                throw new InvalidOperationException("could not get desktop lifetime for application");
+            }
+
+            lifetime.Shutdown();
+        });
     }
 }
