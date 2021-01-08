8fb58b26fed9f09c1278095ce4a1dfcb0996f778
xdrie <xdrie@alt.icu>
2021-01-07 22:38:27 -0800

    add bindings for loading files

--- src/BeanPad/BeanPad/ViewModels/Pages/HomePageVM.cs
+++ src/BeanPad/BeanPad/ViewModels/Pages/HomePageVM.cs
@@ -1,19 +1,45 @@
 using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using AvaloniaEdit.Document;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace BeanPad.ViewModels.Pages {
     public class HomePageVM : ViewModelBase {
         public string Greeting => "Hello, welcome to the world!";
+        [Reactive] public TextDocument EditorDocument { get; set; } = new();
+        public ICommand MenuExit { get; } = ReactiveCommand.Create(() => { getLifetime().Shutdown(); });
+        public ICommand MenuOpenFile { get; }
+        public ICommand MenuSave { get; }
+        public ICommand MenuSaveAs { get; }
 
-        public ICommand MenuExit { get; } = ReactiveCommand.Create(() => {
-            var lifetime = App.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime;
+        public HomePageVM() {
+            MenuOpenFile = ReactiveCommand.CreateFromTask(openFile);
+        }
+
+        private async Task openFile() {
+            var dlg = new OpenFileDialog();
+            var res = await dlg.ShowAsync(getLifetime().MainWindow);
+            var fileName = res.FirstOrDefault();
+            if (fileName != null) {
+                var fileContents = await File.ReadAllTextAsync(fileName);
+                EditorDocument = new TextDocument(fileContents);
+            }
+        }
+
+        private static IClassicDesktopStyleApplicationLifetime getLifetime() {
+            var lifetime = Application.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime;
             if (lifetime == null) {
                 throw new InvalidOperationException("could not get desktop lifetime for application");
             }
 
-            lifetime.Shutdown();
-        });
+            return lifetime;
+        }
     }
 }
